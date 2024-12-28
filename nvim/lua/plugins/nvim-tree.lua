576412d94985be45b03cc15523d975955e655535
Mthy41 <mathmyself@outlook.com>
2024-12-28 18:04:15 -0300

    added new config to nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,6 +6,20 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+      require("nvim-tree").setup({
+      sort = {
+        sorter = "case_sensitive",
+      },
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = true,
+        custom = { "^.git$" },
+      },
+    })
   end,
 }
