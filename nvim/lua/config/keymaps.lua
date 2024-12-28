02c76e35eb7441b80a2148d3297f88e3a33d1f63
Mthy41 <mathmyself@outlook.com>
2024-12-28 17:45:00 -0300

    added new config to plugins

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,16 +4,24 @@ local nrmap = {noremap = true}
 local slnt = {silent = true}
 
 
+-- ==================== general ==================
 -- save and exit
 vks({"n", "i", "v"}, "<C-s>", "<Esc>:w<CR>", slnt, nrmap) -- save
 vks("n", "<C-q>", ":q<CR>", slnt, nrmap) -- exit
 
 -- copy and paste external clipboard
 vks({"v", "x"}, "<leader>y", [["+y]], slnt, nrmap) -- copy to system clipboard
-vks({"v", "x"}, "<leader>p", [["+p]], slnt, nrmap) -- paste from system clipboard
+vks({"v", "x"}, "<leader>p", [["+p]], slnt, nrmap) -- paste from system clipboard (in case Ctrl+Shift+C doesn't work)
 -- vks("v", "<C-S-c>", ":q<CR>", {noremap = true})
 
 -- outro
 vks("n", "<Esc>", ":nohl<CR>", slnt) -- remove search highlights
 vks("i", "<C-BS>", "<C-w>") -- back delete text with ctrl backspace
 vks("n", "<leader>x", ":ToggleCheckbox<CR>", slnt) -- toggle checkbox
+
+-- ================ plugin related ================
+-- Neogit
+vks({"n", "v"}, "<leader>g", ":<Esc>:Neogit<CR>", slnt) -- open Neogit tab
+
+-- Neotree
+vks({"n", "v"}, "<leader>t", ":NvimTreeToggle<CR>", slnt)
