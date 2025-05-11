c1fa356a1cb9277e4b99fed3a9bb091d7c7c8bed
Mthy41 <mathmyself@outlook.com>
2025-05-11 19:34:55 -0300

    added new lang support for nvim-treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = {"javascript", "c"},
+            ensure_installed = {"javascript", "typescript", "c", "rust"},
             highlight = {
                 enable = true,
                     -- disable when open big files
